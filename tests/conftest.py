717ca7cd92a60511c7bb24c8cc45f73e05cb8a33
fhdalsjd <josefrp.ablo@gmail.com>
2026-09-10 11:18:45 +0300

    Add extracted telegram-ad-bot project

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import os
+os.environ.setdefault("BOT_TOKEN","123456:dummy-token-for-tests")
+os.environ.setdefault("ADMIN_ID","1")
+os.environ.setdefault("API_ID","12345")
+os.environ.setdefault("API_HASH","dummy-hash-for-tests")
+os.environ.setdefault("DATABASE_URL","sqlite:///:memory:")
+os.environ.setdefault("TELETHON_SESSION","sessions/test_session")
